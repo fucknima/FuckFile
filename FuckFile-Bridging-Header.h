7c7e3edeb4780e37047e5ffebf723dc3ca160397
fucknima <fucknima@users.noreply.github.com>
2026-08-24 12:28:14 +0800

    Bridging header: include tree_sitter/parser.h so TSLanguage (defined only there) is importable by Swift

--- FuckFile-Bridging-Header.h
+++ FuckFile-Bridging-Header.h
@@ -3,4 +3,5 @@
 // 注意：这份头文件同时被 vendored Runestone 的「import TreeSitter」依赖，
 // 因此这里只放 C 层声明，不引 ObjC 业务头。
 #include <tree_sitter/api.h>
+#include <tree_sitter/parser.h>
 #include "third_party/tree-sitter/grammars.h"
