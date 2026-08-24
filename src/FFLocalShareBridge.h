911b35c6f648bcff5e6b5a4f3c29d8fe4009d8f0
fucknima <99715422@qq.com>
2026-08-24 23:11:30 +0800

    Scope loopback shares to one extension session

--- src/FFLocalShareBridge.h
+++ src/FFLocalShareBridge.h
@@ -17,6 +17,7 @@ NS_ASSUME_NONNULL_BEGIN
 @end
 
 FOUNDATION_EXPORT BOOL FFLocalShareBridgeSendInbox(NSString *inboxPath,
+                                                   NSString *sessionID,
                                                    NSString *token,
                                                    NSUInteger * _Nullable sentCount,
                                                    NSError * _Nullable * _Nullable error);
