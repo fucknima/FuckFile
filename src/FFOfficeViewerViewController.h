c9741808aa66533acf37a7895f3328034b75291f
fucknima <99715422@qq.com>
2026-08-25 23:21:24 +0800

    Add Office viewer interface

---
+++ src/FFOfficeViewerViewController.h
@@ -0,0 +1,9 @@
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FFOfficeViewerViewController : UIViewController
+- (instancetype)initWithFilePath:(NSString *)filePath;
+@end
+
+NS_ASSUME_NONNULL_END
