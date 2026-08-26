4053c649fb780d67ced123f8fe441ab9607a5407
fucknima <99715422@qq.com>
2026-08-26 11:22:25 +0800

    Remove redundant custom Office viewer

--- src/FFOfficeViewerViewController.h
+++
@@ -1,9 +0,0 @@
-#import <UIKit/UIKit.h>
-
-NS_ASSUME_NONNULL_BEGIN
-
-@interface FFOfficeViewerViewController : UIViewController
-- (instancetype)initWithFilePath:(NSString *)filePath;
-@end
-
-NS_ASSUME_NONNULL_END
