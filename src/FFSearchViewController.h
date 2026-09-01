194e19d87e018692ba8a64e77b7de4ee804d2eea
fucknima <99715422@qq.com>
2026-09-01 15:07:14 +0800

    Search: remove orphan global search header

--- src/FFSearchViewController.h
+++
@@ -1,7 +0,0 @@
-#import <UIKit/UIKit.h>
-
-// Global recursive search over the MHA virtual root, with search
-// history on the idle screen and live batched results.
-@interface FFSearchViewController : UITableViewController
-
-@end
