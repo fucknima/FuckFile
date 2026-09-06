4fea8d195069415146bdb8523c4f72f8203481f3
fucknima <99715422@qq.com>
2026-09-06 09:35:59 +0800

    Archive: correct libarchive bridge signatures

--- src/FFLibArchiveBackend.h
+++ src/FFLibArchiveBackend.h
@@ -11,20 +11,20 @@ FOUNDATION_EXPORT BOOL FFLibArchiveBackendAvailable(void);
 
 FOUNDATION_EXPORT NSArray<FFArchiveEntry *> * _Nullable
 FFLibArchiveListEntries(NSString *archivePath,
-                        NSString * _Nullable *password,
+                        NSString * _Nullable password,
                         NSError * _Nullable * _Nullable error);
 
-FOUNDATION_EXPORT NSString * _Nullable *
+FOUNDATION_EXPORT NSString * _Nullable
 FFLibArchiveExtractEntry(NSString *entryName,
                          NSString *archivePath,
                          NSString *destinationDirectory,
-                         NSString * _Nullable *password,
+                         NSString * _Nullable password,
                          NSError * _Nullable * _Nullable error);
 
 FOUNDATION_EXPORT BOOL
 FFLibArchiveExtractAll(NSString *archivePath,
                        NSString *destinationDirectory,
-                       NSString * _Nullable *password,
+                       NSString * _Nullable password,
                        NSArray<NSString *> * _Nullable * _Nullable entryNames,
                        void (^ _Nullable progressBlock)(double progress, NSString *entryName),
                        BOOL (^ _Nullable shouldCancel)(void),
