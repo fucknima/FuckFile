0c713b3cf5fe51acc53c70282c60c693b0bb7de6
fucknima <99715422@qq.com>
2026-09-08 00:40:54 +0800

    Viewer: add Mach-O inspector interface

---
+++ src/FFMachOInspectorViewController.h
@@ -0,0 +1,9 @@
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FFMachOInspectorViewController : UITableViewController
+- (nullable instancetype)initWithFilePath:(NSString *)path;
+@end
+
+NS_ASSUME_NONNULL_END
